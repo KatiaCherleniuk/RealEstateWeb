5a3c5c6e9fc4ae99bdffe9cce5a0aae349ca65f5
Katia <katia.cherleniuk@gmail.com>
2023-12-14 18:38:28 +0200

    Changed styles. Added form.

--- js/map.js
+++ js/map.js
@@ -3,5 +3,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([50.7463998, 25.3285528]).addTo(map);
+map.scrollWheelZoom.disable();
+
+var markerIcon = L.icon({
+    iconUrl: '../img/marker.png',
+
+    iconSize:     [30, 50], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+var marker = L.marker([50.7463998, 25.3285528],  {icon: markerIcon}).addTo(map);
 marker.bindPopup("<b>м. Луцьк</b><br>Проспект Волі 9").openPopup();
